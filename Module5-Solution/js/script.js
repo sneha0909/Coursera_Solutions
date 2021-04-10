09508c8c3f046833a08bbcc7036da1a38e9a0dc8
Sneha Shukla <snehadefence090998@gmail.com>
2021-04-11 04:21:51 +0700

    try once again

--- Module5-Solution/js/script.js
+++ Module5-Solution/js/script.js
@@ -84,7 +84,11 @@ showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   //[...],
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  //buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
